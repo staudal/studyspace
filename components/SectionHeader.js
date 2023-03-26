21c6d5051fdb3d22adc2cd970767e9a17f4ee20f
Jakob Staudal <jakob@staudal.dev>
2023-03-26 11:38:58 +0200

    Fixed blue section header

--- components/SectionHeader.js
+++ components/SectionHeader.js
@@ -1,7 +1,9 @@
 export default function SectionHeader({ title }) {
   return (
-    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-indigo-500 border-b text-center sm:py-10 py-8'>
-      <h2 className='text-2xl font-bold text-white sm:text-3xl'>{title}</h2>
+    <div className='bg-indigo-500'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center sm:py-10 py-8'>
+        <h2 className='text-2xl font-bold text-white sm:text-3xl'>{title}</h2>
+      </div>
     </div>
   )
 }
