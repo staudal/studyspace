752c9b57b3c82059ad05db014c9dbe61e7401fbc
Jakob Staudal <jakob@staudal.dev>
2023-03-28 23:55:22 +0200

    Maybe fixed middleware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { hasCookie, setCookie } from 'cookies-next'
 
 export function middleware(req) {
-  const verified = hasCookie('next-auth.session-token') || hasCookie('__Secure-next-auth.session-token')
+  let sessionCookie = req.cookies.has('next-auth.session-token') || req.cookies.has('__Secure-next-auth.session-token')
 
   if (req.nextUrl.pathname.startsWith('/fag')) {
-    if (!verified) {
+    if (!sessionCookie) {
       const url = req.nextUrl.clone()
       url.pathname = '/auth/signin'
       return NextResponse.rewrite(url)
