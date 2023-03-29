d6ca4e3130a59a63d977c6b58dbf31c79c1d5b89
Jakob Staudal <jakob@staudal.dev>
2023-03-29 11:16:58 +0200

    Fixed signout button on mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -148,7 +148,13 @@ export default function Example() {
               </div>
               <div className='mt-3 space-y-1 px-2 sm:px-3'>
                 {userNavigation.map((item) => (
-                  <Disclosure.Button key={item.name} as='a' href={item.href} className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'>
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+                    onClick={item.onClick != undefined ? item.onClick : null}
+                  >
                     {item.name}
                   </Disclosure.Button>
                 ))}
